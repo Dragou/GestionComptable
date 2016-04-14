2d0abd062fb1df16f8e175d53294190d7bdb9359
elisepoirier <elisepoirier@outlook.com>
2016-04-14 12:14:49 +0200

    Ajout des attributs dans la class Actions

--- src/Main/Actions.java
+++ src/Main/Actions.java
@@ -1,5 +1,17 @@
 package Main;
 
-public class Actions {
+import java.util.Date;
 
+public class Actions {
+	private int id;
+	private String categorie;
+	private String type;
+	private float amount;
+	private Date date;
+	private Boolean validate;
+	private String automated;
+	
+	public Action(){
+		
+	}
 }
